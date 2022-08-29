5c4e0deab33b2561ca51cea3b1e0959116b7f4d3
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-08-29 16:33:06 +0200

    Finish Default display screen Scenario
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,16 @@
 function loadCalculator () {
+    const buttons = document.getElementsByClassName('button')
+
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains('highlighted')) {
+            buttons[i].classList.remove('highlighted')
+        }
+        if (buttons[i].classList.contains('disabled')) {
+            buttons[i].classList.remove('disabled')
+        }
+    }
+
     document.getElementById('result').value = 0
+    document.getElementById('+/-').classList.add('disabled')
+    document.getElementById('0').classList.add('disabled')
 }
