80966c33c1e56629d014b6aa64da74a7111dac33
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-08-29 18:54:36 +0200

    Finish Pressing non-operators keys Scenario and split actions to separate functions
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,6 @@
 function loadCalculator () {
-    const buttons = document.getElementsByClassName('button')
+    clearResult()
 
-    for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].classList.contains('highlighted')) {
-            buttons[i].classList.remove('highlighted')
-        }
-        if (buttons[i].classList.contains('disabled')) {
-            buttons[i].classList.remove('disabled')
-        }
-    }
-
-    document.getElementById('result').value = 0
     document.getElementById('+/-').classList.add('disabled')
     document.getElementById('0').classList.add('disabled')
 
@@ -27,51 +17,85 @@ function addEvents () {
         if (button.classList.contains('number')) {
             if (button.id === ',') {
                 button.addEventListener('click', function () {
-                    result.value += ','
+                    addPointToResult()
                 })
             } else {
                 button.addEventListener('click', function () {
-                    if (result.value[0] === '0' && result.value.length === 1) {
-                        result.value = button.id
-                    } else {
-                        result.value += button.id
-                    }
+                    addNumberToResult(button.id)
                 })
             }
         } else if (button.classList.contains('operator')) {
             if (button.id === 'C') {
                 button.addEventListener('click', function () {
-                    result.value = 0
-                    unHighlight()
+                    clearResult()
                 })
             } else if (button.id === '+/-') {
                 button.addEventListener('click', function () {
-                    result.value = result.value * -1
+                    changeResultSign()
                 })
             } else if (button.id === '+' || button.id === '-' || button.id === '*' || button.id === '/') {
                 button.addEventListener('click', function () {
-                    highlight(button)
+                    highlightKey(button)
                 })
             } else if (button.id === '=') {
                 button.addEventListener('click', function () {
-                    unHighlight()
+                    unHighlightKeys()
                 })
             }
         }
     }
 
-    function highlight (button) {
-        unHighlight()
-        button.classList.add('highlighted')
+    document.addEventListener('keydown', function (event) {
+        console.log(event)
+
+        if (event.key === ',') {
+            addPointToResult()
+        } else if (event.key >= 0 && event.key <= 9) {
+            addNumberToResult(event.key)
+        } else if (event.key === 'Escape') {
+            clearResult()
+        } else if (event.key === 'Control') {
+            changeResultSign()
+        }
+    })
+}
+
+function clearResult () {
+    unHighlightKeys()
+    document.getElementById('result').value = 0
+}
+
+function addPointToResult () {
+    document.getElementById('result').value += ','
+}
+
+function addNumberToResult (number) {
+    const result = document.getElementById('result')
+
+    if (result.value[0] === '0' && result.value.length === 1) {
+        result.value = number
+    } else {
+        result.value += number
     }
+}
 
-    function unHighlight () {
-        const buttons = document.getElementsByClassName('button')
+function changeResultSign () {
+    const result = document.getElementById('result')
 
-        for (let i = 0; i < buttons.length; i++) {
-            if (buttons[i].classList.contains('highlighted')) {
-                buttons[i].classList.remove('highlighted')
-            }
+    result.value = result.value * -1
+}
+
+function highlightKey (button) {
+    unHighlightKeys()
+    button.classList.add('highlighted')
+}
+
+function unHighlightKeys () {
+    const buttons = document.getElementsByClassName('button')
+
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].classList.contains('highlighted')) {
+            buttons[i].classList.remove('highlighted')
         }
     }
 }
