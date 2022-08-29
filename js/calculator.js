4e461c8978c9e405a21c1d8f59ff6a69da66f4aa
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-08-29 17:24:32 +0200

    Finish Clicking non-operators screen buttons Scenario
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- js/calculator.js
+++ js/calculator.js
@@ -13,4 +13,41 @@ function loadCalculator () {
     document.getElementById('result').value = 0
     document.getElementById('+/-').classList.add('disabled')
     document.getElementById('0').classList.add('disabled')
+
+    addEvents()
+}
+
+function addEvents () {
+    const buttons = document.getElementsByClassName('button')
+    const result = document.getElementById('result')
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i]
+
+        if (button.classList.contains('number')) {
+            if (button.value === ',') {
+                button.addEventListener('click', function () {
+                    result.value += ','
+                })
+            } else {
+                button.addEventListener('click', function () {
+                    if (result.value[0] === '0' && result.value.length === 1) {
+                        result.value = button.value
+                    } else {
+                        result.value += button.value
+                    }
+                })
+            }
+        } else if (button.classList.contains('operator')) {
+            if (button.value === 'C') {
+                button.addEventListener('click', function () {
+                    result.value = 0
+                })
+            } else if (button.value === '+/-') {
+                button.addEventListener('click', function () {
+                    result.value = result.value * -1
+                })
+            }
+        }
+    }
 }
